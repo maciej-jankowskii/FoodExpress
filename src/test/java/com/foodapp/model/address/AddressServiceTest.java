fb6600f2ca890fa55b29607bfb6ad411ab061eec
Maciej Jankowski <jankowski.maciej0@gmail.com>
2023-11-07 22:12:04 +0100

    First tests added.

---
+++ src/test/java/com/foodapp/model/address/AddressServiceTest.java
@@ -0,0 +1,63 @@
+package com.foodapp.model.address;
+
+import com.foodapp.model.user.User;
+import com.foodapp.model.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+class AddressServiceTest {
+    @Mock private UserRepository userRepository;
+    @Mock private AddressMapper addressMapper;
+    @Mock private AddressRepository addressRepository;
+    @InjectMocks private AddressService addressService;
+
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Should add address")
+    public void addAddressForUser_Success_Test(){
+        AddressDTO dto = new AddressDTO();
+        Address address = new Address();
+        User user = new User();
+        String email = "test@example.com";
+        user.setEmail(email);
+
+        when(userRepository.findUserByEmail(email)).thenReturn(Optional.of(user));
+        when(addressMapper.map(dto)).thenReturn(address);
+        when(addressRepository.save(address)).thenReturn(address);
+        when(userRepository.save(user)).thenReturn(user);
+        when(addressMapper.map(address)).thenReturn(dto);
+        AddressDTO result = addressService.addAddressForUser(email, dto);
+
+        assertNotNull(result);
+        assertEquals(dto, result);
+        assertDoesNotThrow(() -> addressService.addAddressForUser(email, dto));
+
+    }
+
+    @Test
+    @DisplayName("Should thorw exception")
+    public void addAddressForUser_Exception_Test(){
+        AddressDTO addressDTO = new AddressDTO();
+        String email = "test@example.com";
+
+        when(userRepository.findUserByEmail(anyString())).thenReturn(Optional.empty());
+        assertThrows(UsernameNotFoundException.class,
+                () -> addressService.addAddressForUser(email, addressDTO));
+    }
+
+}
